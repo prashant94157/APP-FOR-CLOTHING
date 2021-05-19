0e55d8f4094270091d58aa25dbb4638327e5ef9c
prashant94157 <0166621m@gmail.com>
2021-05-20 00:20:42 +0530

    completed checkout page

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
+
 import { selectCurrentUser } from './redux/user/user.selectors';
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -53,6 +59,7 @@ class App extends React.Component {
             <SignInAndSignUpPage />
             )
           } />
+          <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
     );
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,8 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce((count , cartItem)=>count + cartItem.quantity,0)
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((count , cartItem)=>count + cartItem.quantity * cartItem.price,0)
+);
